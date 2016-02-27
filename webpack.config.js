3f47ad75bdc7c6c06cf532981d0bf6deb6a532a5
baldwinjj <baldwinjj@g.cofc.edu>
2016-02-27 12:05:57 -0500

    Finish setting up the development environment

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+  app: ['webpack/hot/dev-server', './app/entry.js'],
+},
+
+output: {
+  path: './public/built',
+  filename: 'bundle.js',
+  publicPath: 'http://localhost:8080/built/'
+},
+
+devServer: {
+  contentBase: './public',
+  publicPath: 'http://localhost:8080/built/'
+},
+
+module: {
+ loaders: [
+   {
+     test: /\.js$/,
+     loader: 'babel-loader',
+     exclude: /node_modules/,
+     query: {
+       presets:['es2015','react']
+     }
+   }
+ ]
+},
+
+ plugins: [
+   new webpack.HotModuleReplacementPlugin()
+ ]
+}
